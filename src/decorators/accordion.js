53d514d225fac6e188682b89143e15c6caa27b0f
Roman <kaboom.ua@gmail.com>
2016-08-22 22:57:39 +0300

    Update accordion.js

--- src/decorators/accordion.js
+++ src/decorators/accordion.js
@@ -2,6 +2,8 @@
 //decorator
 import React from 'react'
 
+//Декораторы создают для переиспользования кода. Их следует делать максимеально универсальными, 
+//не привязывайся к названиям сущностей типа Article. Лучше называй toggleOpenItem, например
 export default (Component) => {
     return class AccordionDecorator extends React.Component {
         state = {
