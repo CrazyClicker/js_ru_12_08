7797a0dcd834ad91210b438ac34a5df7eeaede2e
Roman <kaboom.ua@gmail.com>
2016-08-22 22:59:57 +0300

    Update ArticleList.js

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -4,11 +4,12 @@ import accordion from '../decorators/accordion'
 
 class ArticleList extends Component {
     static propTypes = {
+        //теперь компонент расчитывает на isOpen и toggleOpen - рекомендую их тоже описать
         articles: PropTypes.arrayOf(PropTypes.object)
     }
 
     render() {
-
+        //мне лично надоедает писать this.props по этому разбиваю на переменные
         const articleItems = this.props.articles.map(articleObject =>
             <li key = {articleObject.id}>
                 <Article article = {articleObject}
