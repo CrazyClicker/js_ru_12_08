f6b84cd7076eaf9ae9ac6d9428916e43c61b0354
Clicker <crazyclicker@gmail.com>
2016-08-25 18:33:08 +0300

    callback

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,10 +1,13 @@
 import React, { Component, PropTypes } from 'react'
 import ArticleList from './ArticleList'
 import Select from 'react-select'
-import DatePicker from './DatePicker'
-import 'react-select/dist/react-select.css'
+import DayPicker, { DateUtils } from 'react-day-picker';
 import JqueryComponent from './JqueryComponent'
 import { findDOMNode } from 'react-dom'
+import moment from 'moment';
+
+import 'react-day-picker/lib/style.css';
+import 'react-select/dist/react-select.css'
 
 class Container extends Component {
     static propTypes = {
@@ -12,6 +15,8 @@ class Container extends Component {
     };
 
     state = {
+        from: null,
+        to: null,
         selected: null,
     }
 
@@ -20,9 +25,25 @@ class Container extends Component {
             label: article.title,
             value: article.id
         }))
+
+        const {from, to} = this.state
         return (
             <div>
-                <DatePicker />
+                <DayPicker
+                    ref="daypicker"
+                    numberOfMonths={ 2 }
+                    selectedDays={ day => DateUtils.isDayInRange(day, { from, to }) }
+                    onDayClick={ this.setRange }
+                />
+                <div>
+                    { !from && !to && <p>Please select the <strong>first day</strong>.</p> }
+                    { from && !to && <p>Please select the <strong>last day</strong>.</p> }
+                    { from && to &&
+                    <p>
+                        You chose from { moment(from).format('L') } to { moment(to).format('L') }
+                    </p>
+                    }
+                </div>
                 <Select options = {options} value={this.state.selected} onChange = {this.handleChange} multi={true}/>
                 <ArticleList articles = {this.props.articles} />
                 <JqueryComponent items = {this.props.articles} ref={this.getJQ}/>
@@ -35,6 +56,11 @@ class Container extends Component {
         console.log('---', findDOMNode(ref))
     }
 
-}
+    setRange = (e, day) => { //возможно я не понял задачи и надо было через ref делать
+        const range = DateUtils.addDayToRange(day, this.state);
+        this.setState(range);
+    }}
+
+
 
 export default Container
