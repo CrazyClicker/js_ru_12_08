4f35106647b1035bd0a415257a23ffd15bf88ade
Clicker <crazyclicker@gmail.com>
2016-09-05 22:30:05 +0300

    comment

--- src/AC/comments.js
+++ src/AC/comments.js
@@ -1,6 +1,9 @@
 import { ADD_COMMENT } from '../constants'
 //import { v4 } from 'node-uuid'
 
+/* Не стал выносить генерацию id в middleware потому что мне эта идея не нравится примерно так же как триггеры в sql.
+Кога пишешь выходит красиво, а потом является причиной тучи баггов пока не всплывает мысль "А у меня же там триггер"
+  */
 let commentId = 999;
 
 export default function addComment(text, articleId) {
